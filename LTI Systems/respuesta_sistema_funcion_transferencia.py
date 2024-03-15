24c0595f16a54e48be7fb94457fc75074cfaf584
rtachiquing <108795509+rtachiquing@users.noreply.github.com>
2024-03-15 16:14:22 -0600

    Update respuesta_sistema_funcion_transferencia.py

--- LTI Systems/respuesta_sistema_funcion_transferencia.py
+++ LTI Systems/respuesta_sistema_funcion_transferencia.py
@@ -1,47 +1,146 @@
 from scipy import signal
 import sympy
 
+
+"""
+Ejemplo:
+    
+d c(t)
+------ + 2 c(t) = r(t)
+ d t
+
+
+sC(s) + 2C(s) = R(s)
+
+
+        C(s)       1
+G(s) = ------ = -------
+        R(s)     s + 2 
+
+"""
 t, s = sympy.symbols('t, s')
 a = sympy.symbols('a', real=True, positive=True)
 
-
 num = [1]
-den = [1, 2, 0, 0]
+den = [1, 2]
 r,p,k = signal.residue(num, den)
 print("CASO 1: ", end="")
 print(r,p,k)
 print("Unique Roots --> ", end="")
 uniq, mult = signal.unique_roots(p)
 print(uniq, mult, end="")
-
-F = []
-for i in range(len(uniq)):
-    for j in range(mult[i],0,-1):
-        F_fracc = r[i]/((s-p[i]))**j 
-        F.append(F_fracc)
-    
-for i in F:
-    print(sympy.inverse_laplace_transform(i, s, t), end="")
-    print(" (+/-) ", end="")
 print()
 
+F = []
+k = 0
+for i in mult:
+    for j in range(i):
+        F_fracc = r[k]/((s-p[k])**(j+1)) 
+        F.append(F_fracc)
+        k += 1
 
-f = 0.5*sympy.exp(-4*t) 
-F1 = sympy.laplace_transform(f, t, s, noconds=True)
-f1 = sympy.inverse_laplace_transform(F1, s, t)
-f2 = 0.5 - f
-f3 = (-1/4) + t/2 + (sympy.exp(-2*t)/4)
+print(F)
+    
+f_original = sympy.exp(-2*t)
+f_conv_impulso = 0
+for i in F:
+    f_conv_impulso += sympy.inverse_laplace_transform(i, s, t)
 
-p = sympy.plot(f.subs({a: 2}), f1.subs({a: 2}), f2.subs({a: 2}), f3.subs({a: 2}), 
-               xlim=(-1, 5), ylim=(0, 3), show=False)
-p[1].line_color = 'red'
-p[2].line_color = 'orange'
-p[3].line_color = "purple"
-p.show()
+print(f_conv_impulso)
+print()
+
+plot = sympy.plot(f_original, f_conv_impulso, xlim=(-1, 5), ylim=(0, 3), show=False)
+plot[1].line_color = 'red'
+plot.show()
 
 
+"""
+Ejemplo:
 
-from sympy.physics.control.lti import TransferFunction
-tf1 = TransferFunction(1, s**2 + 2*s, s)
-print(tf1.num)
-print(tf1.den)
+        C(s)       1
+G(s) = ------ = -------
+        R(s)     s + 2 
+
+                                                            1
+C(s) = {Escalón Unitario = R(s)}  * G(s) = R(s)*G(s) = -----------
+                                                        s (s + 2)
+"""
+num = [1]
+den = [1, 2, 0]
+r,p,k = signal.residue(num, den)
+print("CASO 1: ", end="")
+print(r,p,k)
+print("Unique Roots --> ", end="")
+uniq, mult = signal.unique_roots(p)
+print(uniq, mult, end="")
+print()
+
+F = []
+k = 0
+for i in mult:
+    for j in range(i):
+        F_fracc = r[k]/((s-p[k])**(j+1)) 
+        F.append(F_fracc)
+        k += 1
+
+print(F)
+    
+
+f_conv_esc_unit = 0
+for i in F:
+    f_conv_esc_unit += sympy.inverse_laplace_transform(i, s, t)
+
+print(f_conv_esc_unit)
+print()
+
+plot = sympy.plot(f_original, f_conv_impulso, f_conv_esc_unit, 
+                  xlim=(-1, 5), ylim=(0, 3), show=False)
+plot[1].line_color = 'red'
+plot[2].line_color = 'orange'
+plot.show()
+
+"""
+Ejemplo:
+
+        C(s)       1
+G(s) = ------ = -------
+        R(s)     s + 2 
+
+                                                           1
+C(s) = {Rampa Unitaria = R(s)}  * G(s) = R(s)*G(s) = -------------
+                                                      s^2 (s + 2)
+"""
+num = [1]
+den = [1, 2, 0, 0]
+r,p,k = signal.residue(num, den)
+print("CASO 2: ", end="")
+print(r,p,k)
+print("Unique Roots --> ", end="")
+uniq, mult = signal.unique_roots(p)
+print(uniq, mult, end="")
+print()
+
+F = []
+k = 0
+for i in mult:
+    for j in range(i):
+        F_fracc = r[k]/((s-p[k])**(j+1)) 
+        F.append(F_fracc)
+        k += 1
+
+print(F)
+
+
+f_conv_ramp_unit = 0
+for i in F:
+    f_conv_ramp_unit += sympy.inverse_laplace_transform(i, s, t)
+
+print(f_conv_ramp_unit)
+print()
+
+plot = sympy.plot(f_original, f_conv_impulso, f_conv_esc_unit, f_conv_ramp_unit,
+                  xlim=(-1, 5), ylim=(0, 3), show=False)
+plot[1].line_color = 'red'
+plot[2].line_color = 'orange'
+plot[3].line_color = 'purple'
+plot.show()
